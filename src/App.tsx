d70c66ddccc0fbdde4ab3ca674a56197b956054b
Alan Gabriel <contatoalangabriel@gmail.com>
2022-07-27 17:53:20 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import CardSkeleton from 'components/Card/skeleton';
 import ApplicationContext from 'hooks/useTheme';
 import { useTranslation } from 'react-i18next';
 import { ThemeProvider } from 'styled-components';
@@ -14,7 +13,6 @@ function App() {
       <ThemeProvider theme={theme}>
         <GlobalStyles />
         <p>{t('welcome_to_app')}</p>
-        <CardSkeleton />
       </ThemeProvider>
     </ApplicationContext>
   );
