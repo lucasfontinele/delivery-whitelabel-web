4167ebc54f0c99a3e3e99f508380c011a9cee09b
Lucas <lfontinele07@gmail.com>
2022-08-04 11:43:22 -0300

    TextArea component changes

--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -1,14 +1,14 @@
 import type { TextAreaProps } from './TextArea.misc';
-import styles from './TextArea.styles.module.scss';
+import * as S from './styles';
 
 function TextArea({ ...props }: TextAreaProps) {
   const { error } = props;
 
   return (
-    <div className={styles.wrapper}>
-      <textarea className={styles.container} {...props} />
-      <small className={styles.error}>{error}</small>
-    </div>
+    <S.Container>
+      <S.TextArea {...props} />
+      <S.Small>{error}</S.Small>
+    </S.Container>
   );
 }
 
