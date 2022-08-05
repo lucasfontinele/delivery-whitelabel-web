fe3a525e73b5792a61ac5628f0e4407b5cae236a
Lucas <lfontinele07@gmail.com>
2022-08-05 10:31:26 -0300

    Fix: snackbar storybook props

--- src/components/Snackbar/Snackbar.stories.tsx
+++ src/components/Snackbar/Snackbar.stories.tsx
@@ -1,14 +1,13 @@
 import { Story, Meta } from '@storybook/react/types-6-0';
 
 import Snackbar from '.';
-import type { SnackbarProps } from './Snackbar.misc';
 
 export default {
   title: 'Component/Snackbar',
   component: Snackbar,
 } as Meta;
 
-const Template: Story<SnackbarProps> = args => <Snackbar {...args} />;
+const Template: Story = args => <Snackbar {...args} />;
 
 export const Default = Template.bind({});
 
