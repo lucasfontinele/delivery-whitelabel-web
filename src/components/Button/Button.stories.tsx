070aa35437199cf9dd188c2401e41207b4b6ebee
Lucas <lfontinele07@gmail.com>
2022-08-05 10:04:04 -0300

    Hot fixes

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -2,18 +2,13 @@ import { Story, Meta } from '@storybook/react/types-6-0';
 
 import Button from '.';
 import type { ButtonProps } from './Button.misc';
-import styles from './Button.styles.module.scss';
 
 export default {
   title: 'Component/Button',
   component: Button,
 } as Meta;
 
-const Template: Story<ButtonProps> = args => (
-  <div className={styles.storybookContainer}>
-    <Button {...args} />
-  </div>
-);
+const Template: Story<ButtonProps> = args => <Button {...args} />;
 
 export const Default = Template.bind({});
 
