94b070718622f883dd5113e300a93d256a2345ff
Lucas <lfontinele07@gmail.com>
2022-09-09 17:18:51 -0300

    Implements back button component

---
+++ src/components/BackButton/index.tsx
@@ -0,0 +1,13 @@
+import * as S from './styles';
+
+import StickChevronLeft from '../Icons/StickChevronLeft';
+
+function BackButton() {
+  return (
+    <S.Container>
+      <StickChevronLeft />
+    </S.Container>
+  );
+}
+
+export default BackButton;
---
+++ src/components/BackButton/styles.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.button`
+  border: none;
+  background: transparent;
+  padding: 4px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
