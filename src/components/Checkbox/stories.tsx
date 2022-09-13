2dccbd9563068dc1c3881d93290d97a80cec6b55
Lucas <lfontinele07@gmail.com>
2022-09-13 02:57:37 -0300

    Hot fix

--- src/components/Checkbox/stories.tsx
+++ src/components/Checkbox/stories.tsx
@@ -1,14 +1,19 @@
 import { Story, Meta } from '@storybook/react/types-6-0';
 
 import Checkbox from '.';
+import { CheckboxProps } from './misc';
 
 export default {
   title: 'Component/Checkbox',
   component: Checkbox,
 } as Meta;
 
-const Template: Story = args => <Checkbox {...args} />;
+const Template: Story<CheckboxProps> = args => <Checkbox {...args} />;
 
 export const Default = Template.bind({});
 
-Default.args = {};
+Default.args = {
+  onChangeChecked: () => {
+    /* */
+  },
+};
