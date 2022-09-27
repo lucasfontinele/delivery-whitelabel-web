def1d7fd154fe340e1587ce373bbab49c7b040a1
Lucas <lfontinele07@gmail.com>
2022-09-27 00:33:16 -0300

    Implements offers filter

--- src/pages/Offers/index.tsx
+++ src/pages/Offers/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 
 import Card from 'components/Card';
 import CategoriesScroll from 'components/CategoriesScroll';
@@ -30,11 +30,18 @@ function Offers() {
   const isClosed = isClosedStore(opening_schedule);
 
   const [distance, setDistance] = useState('0');
+  const [search, setSearch] = useState('');
 
   const handleDistance = () => {
     const calculatedDistance = calculateDistanceBetween([a, b]);
 
     setDistance(calculatedDistance);
+  };
+
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+
+    setSearch(value);
   };
 
   useEffect(() => {
@@ -64,7 +71,11 @@ function Offers() {
 
           <S.ControlArea disabled={isClosed}>
             <S.SearchContainer>
-              <Search placeholder="Pesquisar" disabled={isClosed} />
+              <Search
+                placeholder="Pesquisar"
+                disabled={isClosed}
+                onChange={handleSearch}
+              />
             </S.SearchContainer>
 
             <S.CategoriesContainer>
@@ -72,27 +83,38 @@ function Offers() {
             </S.CategoriesContainer>
 
             <S.OffersContainer>
-              {offers.map(group => (
-                <S.CategoryContainer key={group.label} id={group.label}>
-                  <S.CategoryTitle>{group.label}</S.CategoryTitle>
+              {offers.map(group => {
+                if (
+                  group.offers.filter(data => data.title.includes(search))
+                    .length <= 0
+                ) {
+                  return undefined;
+                }
 
-                  {group.offers.map(offer => (
-                    <Card
-                      key={offer.id}
-                      description={offer.description}
-                      name={offer.title}
-                      href="#"
-                      price={offer.amount}
-                      promotionalPrice={
-                        offer.amount_without_discount > 0
-                          ? offer.amount_without_discount
-                          : undefined
-                      }
-                      image="https://img.cybercook.com.br/receitas/71/salada-primavera-6.jpeg"
-                    />
-                  ))}
-                </S.CategoryContainer>
-              ))}
+                return (
+                  <S.CategoryContainer key={group.label} id={group.label}>
+                    <S.CategoryTitle>{group.label}</S.CategoryTitle>
+
+                    {group.offers
+                      .filter(data => data.title.includes(search))
+                      .map(offer => (
+                        <Card
+                          key={offer.id}
+                          description={offer.description}
+                          name={offer.title}
+                          href="#"
+                          price={offer.amount}
+                          promotionalPrice={
+                            offer.amount_without_discount > 0
+                              ? offer.amount_without_discount
+                              : undefined
+                          }
+                          image="https://img.cybercook.com.br/receitas/71/salada-primavera-6.jpeg"
+                        />
+                      ))}
+                  </S.CategoryContainer>
+                );
+              })}
             </S.OffersContainer>
           </S.ControlArea>
         </PageCard>
